ba95a3ce20598f1bba08f482926a148e5d36003d
Michael <mizitoh@hotmail.com>
2021-10-25 20:01:32 -0300

    first Commit

---
+++ src/main/java/com/github/mizitoh/citiesapi/states/StateRepository.java
@@ -0,0 +1,6 @@
+package com.github.mizitoh.citiesapi.states;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
